19d34d83d057883f87cb02ac7a8878c290654b15
yusufom <yusufolamilekanmuktar@gmail.com>
2023-08-31 21:10:16 +0100

    update

--- bootcamp_django/core/views.py
+++ bootcamp_django/core/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 # Create your views here.
 
 def home(request):
-    return render(request, 'core/index.html')
+    queryset = Course.objects.all()
+    context = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'queryset': queryset,
+    }
+    return render(request, 'core/index.html', context)
+
+
+
+def login(request):
+    context = {
+    }
+    return render(request, 'core/login.html', context)
+
+
+
